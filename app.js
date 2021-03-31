07ffc10257a626925289354a4a146715c74511a9
Hema Sree Rathnam Machha <machhagusus21069@gmail.com>
2021-03-31 03:21:00 -0500

    slot added

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var OrnamentRouter = require('./routes/Ornament');
 var starRouter = require('./routes/stars');
+var slotRouter = require('./routes/slot');
 
 var app = express();
 
@@ -25,6 +26,7 @@ app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/Ornament', OrnamentRouter);
 app.use('/stars', starRouter);
+app.use('/slot', slotRouter);
 
 
 // catch 404 and forward to error handler
