bf52ff59b161b3c86b75c8ab449e079fd1a77147
Hema Sree Rathnam Machha <machhagusus21069@gmail.com>
2021-03-31 02:39:37 -0500

    added stars

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ var logger = require('morgan');
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var OrnamentRouter = require('./routes/Ornament');
+var starRouter = require('./routes/stars');
 
 var app = express();
 
@@ -23,6 +24,8 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/Ornament', OrnamentRouter);
+app.use('/stars', starRouter);
+
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
